660d90493dd2b13a1981c3475b4e90a54f3db60b
Ryan Anjasmara <ryananjasmara1997@gmail.com>
2020-04-29 11:34:53 +0700

    update wordings and colors constants

--- App/configs/colors.js
+++ App/configs/colors.js
@@ -2,3 +2,4 @@ export const GRAY = '#616161';
 export const ORANGE = 'orange';
 export const RED = 'red';
 export const GREEN = 'green';
+export const BLUE = 'blue';
--- App/configs/words.js
+++ App/configs/words.js
@@ -1,3 +1,20 @@
-export const CONFIRMED = 'Confirmed';
-export const DEATHS = 'Deaths';
-export const RECOVERED = 'Recovered';
+const CONFIRMED = 'Confirmed';
+const DEATHS = 'Deaths';
+const RECOVERED = 'Recovered';
+const HEADER_TITLE = 'COVID - 19 Summary Statistic';
+const APP_DESCRIPTION =
+  'Quick introduction\nThis application was created to assist users in monitoring the updated data of Corona Virus.';
+const DATASOURCE = 'Datasource :';
+const APP_SOURCE_LINK = 'coronavirus-tracker-api.herokuapp.com';
+const SUMMARY_CARD_HEADER = 'Latest Worldwide Statistic';
+
+export default {
+  CONFIRMED,
+  DEATHS,
+  RECOVERED,
+  HEADER_TITLE,
+  APP_DESCRIPTION,
+  APP_SOURCE_LINK,
+  DATASOURCE,
+  SUMMARY_CARD_HEADER,
+};
